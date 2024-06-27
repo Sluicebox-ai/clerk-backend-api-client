351c55d5428bb31c46c8ed61777745f27e630f4a
Adrien Brault <adrien.brault@gmail.com>
2024-06-27 13:00:34 +0200

    fix: add undocument from_oauth_google

--- clerk_backend_api_client/models/create_user_response_200_email_addresses_item_verification_type_3_type_2_strategy.py
+++ clerk_backend_api_client/models/create_user_response_200_email_addresses_item_verification_type_3_type_2_strategy.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class CreateUserResponse200EmailAddressesItemVerificationType3Type2Strategy(str, Enum):
+    FROM_OAUTH_GOOGLE = "from_oauth_google"
     OAUTH_CUSTOM_MOCK = "oauth_custom_mock"
     OAUTH_GOOGLE = "oauth_google"
     OAUTH_MOCK = "oauth_mock"
